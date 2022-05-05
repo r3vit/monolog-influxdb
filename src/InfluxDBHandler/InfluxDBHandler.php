3dbf9624ce3c0cd13acac242ac5bfef6c8ef06b8
r3vit <r3vit@users.noreply.github.com>
2022-05-05 11:31:39 +0200

    Update InfluxDBHandler.php

--- src/InfluxDBHandler/InfluxDBHandler.php
+++ src/InfluxDBHandler/InfluxDBHandler.php
@@ -1,23 +1,22 @@
 <?php
-namespace InfluxDBHandler;
 
-use InfluxDB\Point;
-use Monolog\Logger;
-use Monolog\Handler\AbstractProcessingHandler;
+namespace App\Monolog\Handler;
+
 use InfluxDB\Client;
+use InfluxDB\Database;
 use InfluxDB\Database\RetentionPolicy;
-
+use InfluxDB\Point;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
 
 /**
  * This class is a handler for Monolog, which can be used
- * to write records to an Influx database
+ * to write records to an Influx database.
  *
  * Class InfluxDBHandler
- * @package elardvermeulen\monolog-influxdb\InfluxDBHandler
  */
 class InfluxDBHandler extends AbstractProcessingHandler
 {
-
     /**
      * @var bool defines wether the InfluxDB connection has been initialized
      */
@@ -68,28 +67,22 @@ class InfluxDBHandler extends AbstractProcessingHandler
      */
     private $retention_replication;
 
-
     /**
      * InfluxDBHandler constructor.
-     * @param string $username
-     * @param string $password
-     * @param string $protocol
-     * @param string $host
-     * @param string $port
-     * @param string $db
-     * @param int $level
-     * @param bool $bubble
+     *
+     * @param int    $level
      * @param string $retention_duration
-     * @param int $retention_replication
+     * @param int    $retention_replication
+     *
      * @throws Client\Exception
      */
     public function __construct(
-        string $username,
-        string $password,
-        string $protocol = "influxdb",
-        string $host,
-        string $port,
-        string $db,
+        string $username = 'admin',
+        string $password = 'admin',
+        string $protocol = 'influxdb',
+        string $host = 'influxdb.internal',
+        string $port = '8086',
+        string $db = 'manageritalia',
         $level = Logger::DEBUG,
         bool $bubble = true,
         $retention_duration = '3M',
@@ -103,15 +96,12 @@ class InfluxDBHandler extends AbstractProcessingHandler
         $this->db = $db;
         $this->retention_duration = $retention_duration;
         $this->retention_replication = $retention_replication;
-        $this->connection = Client::fromDSN($this->protocol.'://'.$this->username.':'.$this->password.'@'.$this->host.':'.$this->port.'/'.$this->db);
+        $this->connection = Client::fromDSN(sprintf('%s://%s:%s@%s:%s/%s', $this->protocol, $this->username, $this->password, $this->host, $this->port, $this->db));
         parent::__construct($level, $bubble);
     }
 
-    /**
-     *
-     */
-    private function initialise() {
-
+    private function initialise()
+    {
         // check if a database exists then create it if it doesn't
         $database = $this->connection->getClient()->selectDB($this->db);
 
@@ -122,36 +112,56 @@ class InfluxDBHandler extends AbstractProcessingHandler
     }
 
     /**
-     * @param array $record
      * @return bool|void
+     *
      * @throws \InfluxDB\Database\Exception
      * @throws \InfluxDB\Exception
+     * Example: $logger->info("User succesfully logged in.", array('username'  => 'Peter Doe', 'userid'  => 89));
      */
-    protected function write(array $record):void
+    protected function write(array $record): void
     {
-
         if (!$this->initialised) {
             $this->initialise();
         }
 
-        $tags = array(
-            'level'         => $record['level'],
-            'level_name'    => $record['level_name']
-        );
+        $tags = [
+            'level' => $record['level'],
+            'level_name' => $record['level_name'],
+        ];
         $tags = array_merge($tags, $record['context']);
 
-        $points = array(
-            new Point(
-                $record['channel'],
-                $record['message'],
-                $tags
-            )
-        );
+        // Microseconds
+        list($usec, $sec) = explode(' ', microtime());
+        $timestamp = sprintf('%d%06d', $sec, $usec * 1000000);
+
+        // working: working create an array of points
+        $points = [
+           new Point(
+               $record['channel'], // name of the measurement
+               null, // the measurement value
+               ['source' => $record['channel']], // optional tags
+               ['msg' => $record['message']], // optional additional fields
+               $timestamp // Time precision
+           ),
+       ];
+
+        //    var_dump($record['channel']);
+        //    var_dump($record['message']);
+
+        // $points = array(
+        //     new Point(
+        //         "test_log", // name of the measurement
+        //         null, // the measurement value
+        //         // $record['channel'],
+        //         // $record['message'],
+        //         ['step' => 'logData'], // optional tags
+
+        //         $tags,
+        //         $timestamp
+        //     )
+        // );
 
         // now just write your points like you normally would
-        $result = $this->connection->writePoints($points);
+        $result = $this->connection->writePoints($points, Database::PRECISION_MICROSECONDS);
     }
-
-
 }
-
